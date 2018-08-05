d6ccc314cebd8474f57dd12c9d474874a7b105c6
angelica winkler <caw400@nyu.edu>
2018-08-05 18:40:18 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |supply|
       puts "#{season.capitalize}: #{holiday.capitalize}: #{supply.capitalize}"
+    end
   end
 end
 
