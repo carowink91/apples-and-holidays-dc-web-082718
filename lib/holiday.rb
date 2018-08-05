cbf09ea72e7bd50011bb1c4d0217de9991f590f4
angelica winkler <caw400@nyu.edu>
2018-08-05 17:46:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season|
     if season == :winter
-      season.each do |holiday|
+      season.each do |holiday, supplies|
         holiday.push(supply)
       end
     end
