25eb5a4c784e7c246fa7b0c071eed0c2d6752cfa
angelica winkler <caw400@nyu.edu>
2018-08-05 19:26:01 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
 
     holiday.each do |holiday_name, supply|
 
-      if supply.include?("BBQ") == true
+      if supply.include? ("BBQ") == true
         bbqHolidays << holiday_name
       end
     end
